22cef40ef3a29ae803f0e87fcf25ec6e27c0f461
Andrea Garrido <andrea.garridoo@correoaiep.cl>
2021-10-14 15:57:17 -0300

    modification

--- src/app/components/dashboard/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit} from '@angular/core';
 
 import * as dashboardData from '../../../shared/data/dashboard/dashboard'
+import * as chartData from '../../../shared/data/charts/apex_chart';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,8 +11,8 @@ import * as dashboardData from '../../../shared/data/dashboard/dashboard'
 export class DashboardComponent implements OnInit {
 
   ngOnInit(){}
-    
-    
+
+
   width = '100%';
   height = 400;
   type = "usa";
@@ -27,15 +28,27 @@ export class DashboardComponent implements OnInit {
   public recentCustomersData4 = dashboardData.RecentCustomersData4;
   public recentCustomersData5 = dashboardData.RecentCustomersData5;
 
-  
+
    //Sparkline using ApexCharts
    public apexSparkline = dashboardData.apexSparkline;
    public apexSparkline1 = dashboardData.apexSparkline1;
    public apexSparkline2 = dashboardData.apexSparkline2;
    public apexSparkline3 = dashboardData.apexSparkline3;
    public apexSparkline4 = dashboardData.apexSparkline4;
- 
-   
+
+
+
+  public RandomData = chartData.ApexRandomData;
+  public apexData = chartData.ApexChartData;
+  public barData = chartData.BarData;
+  public stackedbarData = chartData.StackedBarData;
+  public donutData = chartData.DonutChartData;
+  public pieData = chartData.PieChartData;
+  public radicalbarData = chartData.RadialBarCircleData;
+  public radicalmultipleData = chartData.RadialBarCircleMultipleData;
+
 
 }
+
+
 
